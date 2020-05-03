c1dfdfbf500020ff01ad76fbf7a5f51cf69c31ac
JeevanNaikOP <61685578+JeevanNaikOP@users.noreply.github.com>
2020-05-03 11:31:52 +0530

    Create double_char.py

---
+++ codingbat/String/double_char.py
@@ -0,0 +1,12 @@
+Given a string, return a string where for every char in the original, there are two chars.
+
+
+double_char('The') → 'TThhee'
+double_char('AAbb') → 'AAAAbbbb'
+double_char('Hi-There') → 'HHii--TThheerree'
+
+def double_char(str):
+  re=""
+  for i in range(len(str)):
+    re= re+str[i:i+1]*2
+  return re
